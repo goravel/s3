83825b235b682fa8a9a3f3cd10c291533413cdd4
ALMAS <almas1992@126.com>
2025-05-13 12:44:49 +0800

    feat: [#597] add support for package install/uninstall via artisan commands (#171)
    Co-authored-by: maimaiti <maimaiti@archeros.com>

--- facades/s3.go
+++ facades/s3.go
@@ -1,19 +1,16 @@
 package facades
 
 import (
-	"log"
-
 	"github.com/goravel/framework/contracts/filesystem"
 
 	"github.com/goravel/s3"
 )
 
-func S3(disk string) filesystem.Driver {
+func S3(disk string) (filesystem.Driver, error) {
 	instance, err := s3.App.MakeWith(s3.Binding, map[string]any{"disk": disk})
 	if err != nil {
-		log.Fatalln(err)
-		return nil
+		return nil, err
 	}
 
-	return instance.(*s3.S3)
+	return instance.(*s3.S3), nil
 }
