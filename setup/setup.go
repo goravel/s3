e5931f18eae583697ad08443315ad4580ed75d1a
Bowen <hwbrzzl@gmail.com>
2025-10-19 21:34:39 +0800

    feat: optimize install

--- setup/setup.go
+++ setup/setup.go
@@ -29,7 +29,8 @@ func main() {
 				Find(match.Providers()).Modify(modify.Register("&s3.ServiceProvider{}")),
 			modify.GoFile(path.Config("filesystems.go")).
 				Find(match.Imports()).Modify(modify.AddImport("github.com/goravel/framework/contracts/filesystem"), modify.AddImport("github.com/goravel/s3/facades", "s3facades")).
-				Find(match.Config("filesystems.disks")).Modify(modify.AddConfig("s3", config)),
+				Find(match.Config("filesystems.disks")).Modify(modify.AddConfig("s3", config)).
+				Find(match.Config("filesystems")).Modify(modify.AddConfig("default", `"s3"`)),
 		).
 		Uninstall(
 			modify.GoFile(path.Config("app.go")).
@@ -37,7 +38,8 @@ func main() {
 				Find(match.Imports()).Modify(modify.RemoveImport(packages.GetModulePath())),
 			modify.GoFile(path.Config("filesystems.go")).
 				Find(match.Config("filesystems.disks")).Modify(modify.RemoveConfig("s3")).
-				Find(match.Imports()).Modify(modify.RemoveImport("github.com/goravel/framework/contracts/filesystem"), modify.RemoveImport("github.com/goravel/s3/facades", "s3facades")),
+				Find(match.Imports()).Modify(modify.RemoveImport("github.com/goravel/framework/contracts/filesystem"), modify.RemoveImport("github.com/goravel/s3/facades", "s3facades")).
+				Find(match.Config("filesystems")).Modify(modify.AddConfig("default", `""`)),
 		).
 		Execute()
 }
